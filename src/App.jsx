65211da986f649f46629ad9284608edfc8bc8e2a
ToyinJr <juniortoyin@yahoo.com>
2024-11-14 19:27:35 +0100

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './tailwind.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <Navbar />
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+let Navbar = () => {
+    return (
+        <div>
+            <p className="bg-red-100">Navbar</p>
+        </div>
+    )
+}
+
+export default Navbar
